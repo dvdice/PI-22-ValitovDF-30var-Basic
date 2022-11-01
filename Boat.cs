2aa6dfb25c5d53fc018e585c8ae72e0f54dd4111
Valitov.Damir <valitovdamir73@gmail.com>
2022-11-01 11:07:35 +0400

    direction+simple logic

---
+++ Boat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+namespace Boats
+{
+	internal class Boat
+	{
+		/// <summary>
+		/// Скорость
+		/// </summary>
+		public int Speed { get; private set; }
+		/// <summary>
+		/// Вес
+		/// </summary>
+		public float Weight { get; private set; }
+		/// <summary>
+		/// Цвет кузова
+		/// </summary>
+		public Color BodyColor { get; private set; }
+		/// <summary>
+		/// Шаг перемещения автомобиля
+		/// </summary>
+		public float Step => Speed * 100 / Weight;
+		/// <summary>
+		/// Инициализация полей объекта-класса автомобиля
+		/// </summary>
+		/// <param name="speed"></param>
+		/// <param name="weight"></param>
+		/// <param name="bodyColor"></param>
+		/// <returns></returns>
+		public void Init(int speed, float weight, Color bodyColor)
+		{
+			Random rnd = new();
+			Speed = speed <= 0 ? rnd.Next(50, 150) : speed;
+			Weight = weight <= 0 ? rnd.Next(40, 70) : weight;
+			BodyColor = bodyColor;
+		}
+
+	}
+
+}
+
---
+++ Direction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailboatRPP { 
+        /// <summary>
+        /// Направление перемещения
+        /// </summary>
+    internal enum Direction
+    {
+        Up = 1,
+        Down = 2,
+        Left = 3,
+        Right = 4
+    }
+    
+}
